bcbeaafa464f5c70bc42a62cd4f6cb8fa3656363
Manoel Castro <manoelcl@gmail.com>
2022-03-17 18:57:36 +0100

    Code and all basic features completed

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
 const quizSection = document.querySelector(".quiz");
 let counter = 0;
 
+document
+  .querySelector(".button")
+  .addEventListener("click", () => generateQuiz());
+
 const addElements = (questionArray) => {
   //Borramos el posible contenido de la sección
   quizSection.innerHTML = "";
@@ -18,14 +22,11 @@ const addElements = (questionArray) => {
     const li = document.createElement("li");
     li.textContent = answer;
     li.addEventListener("click", (event) => {
-      if (event.target.textContent == questionArray.shift().correct) {
-        counter++;
-      }
-      if (questionArray.length > 0) {
-        addElements(questionArray);
-      } else {
-        gameOver();
-      }
+      li.classList.add("check");
+      [...document.querySelector(".quiz ul").children].forEach((element) => {
+        element.classList.add("unclickable");
+      });
+      manageAnswer(event, questionArray);
     });
     ul.append(li);
   });
@@ -33,21 +34,45 @@ const addElements = (questionArray) => {
   quizSection.append(h2, ul);
 };
 
+const manageAnswer = async (event, questionArray) => {
+  setTimeout(() => {
+    if (event.target.textContent == questionArray.shift().correct) {
+      counter++;
+      event.target.classList.add("true");
+    } else {
+      event.target.classList.add("false");
+    }
+  }, 1000);
+  setTimeout(() => {
+    if (questionArray.length > 0) {
+      addElements(questionArray);
+    } else {
+      gameOver();
+    }
+  }, 1500);
+};
+
 const gameOver = () => {
   quizSection.innerHTML = "";
   const h2 = document.createElement("h2");
   h2.textContent = `Game Over. Tu puntuación es : ${counter} de 10`;
-  quizSection.append(h2);
+  const p = document.createElement("p");
+  p.classList.add("button");
+  p.textContent = "Retake quiz";
+  p.addEventListener("click", () => {
+    generateQuiz();
+  });
+  quizSection.append(h2, p);
 };
 
 const generateQuiz = async () => {
+  counter = 0;
   try {
     //Importamos el json con método fetch
     const response = await fetch("./quiz.json");
 
     if (response.ok) {
       const questions = await response.json();
-      console.log(questions);
       const randomQuestions = [];
       for (let i = 0; i < 10; i++) {
         const rnd = Math.floor(Math.random() * questions.length);
@@ -62,4 +87,3 @@ const generateQuiz = async () => {
     console.log(error);
   }
 };
-generateQuiz();
